39d3ac27a1e1e7281ccd6649eab85eb5bac348fd
ccseyhan <cemalcan.seyhan@deop.ca>
2023-06-06 18:41:25 -0300

    terraform added

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.environment}-rg"
+  location = "East US"
+
+  tags = {
+    environment = var.environment
+  }
+}
+
+resource "azurerm_service_plan" "app_plan" {
+  name                = "${var.environment}-app-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "app_service" {
+  name                = "${var.environment}-ccseyhan-appservice"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.app_plan.id
+
+  site_config {
+    application_stack {
+      docker_image = "ccseyhan/rdicidr"
+      docker_image_tag = "0.1.0"
+    }
+  }
+}
---
+++ infra/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.59.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "sshkey"
+    storage_account_name = "ccseyhan"
+    container_name       = "terraform-backend"
+    key                  = "prod.terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {
+  }
+}
