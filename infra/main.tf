0266c213311d99a4649762136d1e34bf93a67f35
ccseyhan <cemalcan.seyhan@deop.ca>
2023-06-06 19:32:26 -0300

    test

--- infra/main.tf
+++ infra/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg" {
     environment = var.environment
   }
 }
-
+#test
 resource "azurerm_service_plan" "app_plan" {
   name                = "${var.environment}-app-plan"
   resource_group_name = azurerm_resource_group.rg.name
